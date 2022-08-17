6590e71614b778bc5eaa13abfed627edd3a9dcae
hlaingwintun <sv.hlaingwt@gmail.com>
2022-08-18 00:23:18 +0800

    Product Screen Details

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,9 +1,37 @@
 import React from 'react'
+import { Link, useParams } from 'react-router-dom'
+import { Row, Col, Image, ListGroup, Card, Button, ListGroupItem } from 'react-bootstrap'
+import Rating from '../components/Rating'
+import products from '../products'
 
 const ProductScreen = () => {
-  return (
-    <div>Product</div>
-  )
+    let params = useParams();
+    const product = products.find(p => p._id === params.id)
+
+  return <>
+      <Link className='btn btn-light my-3' to='/'>Go Back</Link>
+      <Row>
+        <Col md={6}>
+            <Image src={product.image} alt={product.name} fluid/>
+        </Col>
+        <Col md={3}>
+            <ListGroup variant='flush'>
+              <ListGroup.Item>
+                <h3>{product.name}</h3>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Rating value={product.rating} text={`${product.numReviews} reviews`}></Rating>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                Price: ${product.price}
+              </ListGroup.Item>
+              <ListGroup.Item>
+                Description: {product.description}
+              </ListGroup.Item>
+            </ListGroup>
+        </Col>
+      </Row>
+  </>
 }
 
 export default ProductScreen
